965ba6e28773e4e231554f05cb5d674f12016794
sina <can.ajj@gmail.com>
2025-08-30 22:14:08 +0330

    added search bar

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
 import { useEffect, useState } from "react";
 
 export default function App() {
+  const [selectedCity, setSelectedCity] = useState("");
   const apiKey = "ba608fc197984b34b4a175138253008";
   const apiadd =
     "https://api.geoapify.com/v1/geocode/autocomplete?text=Mosc&apiKey=ca4aa98b402f451aa2a2aec1a218b009";
 
+  const handleSelectCity = function (cityName) {
+    setSelectedCity(() => cityName);
+  };
   return (
     <>
       <header>
-        <Search />
+        <Search onSelectCity={handleSelectCity} />
       </header>
+      <Main selectedCity={selectedCity} />
     </>
   );
 }
-function Search() {
+function Search({ onSelectCity }) {
   const [query, setQuery] = useState("");
 
   const [results, setResults] = useState([]);
@@ -52,18 +57,26 @@ function Search() {
         <button className="search-bar__btn">search</button>
       </div>
       <div className="toolBarContainer">
-        <SearchBar results={results} />
+        <SearchBar results={results} onSelectCity={onSelectCity} />
       </div>
     </div>
   );
 }
 
-function SearchBar({ results }) {
+function SearchBar({ results, onSelectCity }) {
   return (
     <ul className="results-list">
       {results.map((item) => (
-        <li key={item.properties.place_id}>{item.properties.formatted}</li>
+        <li
+          key={item.properties.place_id}
+          onClick={() => onSelectCity(item.properties.city)}
+        >
+          {item.properties.formatted}
+        </li>
       ))}
     </ul>
   );
+}
+function Main({ selectedCity }) {
+  return <h1>{selectedCity}</h1>;
 }
